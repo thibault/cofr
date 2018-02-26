0c1fe5f20c60b163d21d47c8dd557c8c2bad2cca
Thibault Jouannic <thibault@miximum.fr>
2018-02-26 18:28:51 +0100

    Remove useless import

--- trezor_keyval/keyval.py
+++ trezor_keyval/keyval.py
@@ -1,6 +1,6 @@
 import os
 import json
-from binascii import hexlify, unhexlify, Error as BinASCIIError
+from binascii import hexlify, unhexlify
 from collections import MutableMapping
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
