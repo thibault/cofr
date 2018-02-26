c7ed3d13554f700aece7ad0029b198a3fa658499
Thibault Jouannic <thibault@miximum.fr>
2018-02-26 22:39:49 +0100

    Use a `Cmd.cmd` custom shell as a ui

--- trezor_keyval/cli.py
+++ trezor_keyval/cli.py
@@ -1,78 +1,117 @@
 import os
+import cmd
 import click
 
 from .keyval import EncryptedStore
 
 
-@click.group()
-@click.option('-f', '--filepath', required=True, type=click.Path())
-@click.pass_context
-def cli(ctx, filepath):
-    """Store and retrieve sensitive data using Trezor encryption.
+class CofrShell(cmd.Cmd):
+    prompt = 'Cofr> '
+    intro = """
+Welcome to the Cofr shell. Type help or ? for command list.
 
-    This software comes with NO WARRANTY whatsoever. Use it at your own risk.
-
-    Also, this is NOT an official Trezor software.
+This software comes with NO WARRANTY whatsoever. Use it at your own risk.
+Make sure to **backup** your store file.
     """
+
+    def __init__(self, *args, **kwargs):
+        self.store = kwargs.pop('store')
+        super().__init__(*args, **kwargs)
+
+    def emptyline(self):
+        """Do nothing, don't repeat previous command."""
+        pass
+
+    def do_list(self, arg):
+        """List all available keys in the store file."""
+
+        click.echo('Here is the list of all known items:')
+        for key in self.store:
+            click.echo(' - {}'.format(key))
+
+    def do_get(self, arg):
+        """Decrypt and display a value from the store."""
+
+        if arg == '':
+            click.echo('Please, provide the key name to display.')
+            return
+
+        key = arg
+        if key in self.store:
+            click.echo('Please, confirm key decryption on the device.')
+            value = self.store[key]
+            click.echo(value)
+        else:
+            click('There is no such key.')
+
+    def do_put(self, arg):
+        """Encrypt and save a value into the store."""
+
+        if arg == '':
+            click.echo('Please, provide the key name to set.')
+            return
+
+        key = arg
+        if key in self.store:
+            click.echo(
+                'For safety reasons, existing keys cannot be modified.\n'
+                'Remove it first then create it anew.')
+        else:
+            value = click.prompt('Please, provide the value to encrypt')
+            self.store[key] = value
+            click.echo('Done!')
+
+    def do_del(self, arg):
+        """Removes an existing value from the store."""
+
+        if arg == '':
+            click.echo('Please, provide the key name to delete.')
+            return
+
+        key = arg
+        if key in self.store:
+            if click.confirm('Are you sure you want drop that key?'):
+                del self.store[key]
+        else:
+            click.echo('There is no such key.')
+
+    def do_sync(self, arg):
+        """Writes data back to the disk."""
+
+        if self.store.synced:
+            click.echo('No changes were done, there is nothing to write.')
+        else:
+            self.store.sync()
+            click.echo('Changes written back to disk.')
+
+    def do_quit(self, arg):
+        """Exits the shell."""
+
+        if self.store.synced:
+            click.echo('Bye!')
+            return True
+        else:
+            return click.confirm(
+                'The store was modified but not synced to disk. If you exit '
+                'now, all your changes will be lost.\n\nAre you sure you want '
+                'to continue?')
+
+    def postloop(self):
+        self.store.close()
+
+
+@click.command()
+@click.option('-f', '--filepath', required=True, type=click.Path())
+def cli(filepath):
+    """Script entry point. Initialize the shell."""
 
     if not os.path.exists(filepath):
         click.confirm(
             "The given file does not exist. Do you wish to create it?",
             abort=True)
 
+    click.echo('Please, confirm store decryption on the device.')
     store = EncryptedStore(filepath)
-    ctx.obj = store
 
-
-@cli.resultcallback()
-@click.pass_obj
-def callback(obj, res, filepath):
-    obj.close()
-
-
-@cli.command()
-@click.pass_obj
-def list(obj):
-    u"""List existing keys."""
-
-    keys = obj.keys()
-    click.echo('\n'.join(keys))
-
-
-@cli.command()
-@click.argument('key')
-@click.argument('value')
-@click.pass_obj
-def set(obj, key, value):
-    u"""Set the key value."""
-
-    if key in obj:
-        raise click.UsageError(
-            'An existing key cannot be overriden. Remove it first.')
-
-    obj[key] = value
-
-
-@cli.command()
-@click.argument('key')
-@click.pass_obj
-def get(obj, key):
-    u"""Get the key value."""
-
-    value = obj[key]
-    click.echo_via_pager(value)
-
-
-@cli.command()
-@click.argument('key')
-@click.confirmation_option(
-    prompt='Are you sure you want to drop that key? It will be lost forever.')
-@click.pass_obj
-def rm(obj, key):
-    u"""Delete an existing key.
-
-    If the key is not present in the store, we just do nothing.
-    """
-
-    if key in obj:
-        del obj[key]
+    shell = CofrShell(store=store)
+    shell.cmdloop()
